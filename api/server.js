86977a7a5ad07ae5f251e65c0b33d078fdc9ec12
Andreu Monforte <andreump5@gmail.com>
2026-08-18 20:43:30 +0200

    feat: db connection with custom dns and mongoose models

--- api/server.js
+++ api/server.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const dns = require ("node:dns");
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const dns = require("node:dns");
+require("dotenv").config();
+const connectDB = require("./config/db");
 
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 dns.setDefaultResultOrder("ipv4first");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Conectar a la base de datos
+connectDB();
 
 // Middlewares globales
 app.use(cors());
 app.use(express.json());
 
 // Ruta de prueba (health check)
-app.get('/api/health', (req, res) => {
-  res.json({ status: 'ok', message: 'Servidor API funcionando correctamente' });
+app.get("/api/health", (req, res) => {
+  res.json({ status: "ok", message: "Servidor API funcionando correctamente" });
 });
 
 // Arrancar servidor
